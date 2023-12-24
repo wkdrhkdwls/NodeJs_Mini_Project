1ced92ad2c26c655eaa7ffe8d99e6a074a80441a
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-12-24 22:28:29 +0900

    channels 리팩토링

--- routes/channels.js
+++ routes/channels.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const conn = require("../mariadb");
+const { body, param, validationResult } = require("express-validator");
 
 router.use(express.json());
 
@@ -8,88 +10,111 @@ let id = 1;
 
 router
   .route("/")
-  .get((req, res) => {
-    let { userId } = req.body;
-    let channels = [];
-    if (db.size && userId) {
-      db.forEach(function (value, key) {
-        if (value.userId === userId) {
-          channels.push(value);
-        }
+  .get(
+    body("userId").notEmpty().isInt().withMessage("숫자를 입력!"),
+    (req, res) => {
+      const err = validationResult(req);
+      if (!err.isEmpty()) return res.status(400).end(err.array());
+
+      let { userId } = req.body;
+
+      let sql = `SELECT * FROM channels WHERE user_id = ?`;
+
+      conn.query(sql, userId, function (err, results) {
+        if (err) return res.status(400).end();
+
+        if (results.length) res.status(200).json(results);
+        else notFoundChannel(res);
       });
 
-      if (channels.length) {
-        res.status(200).json(channels);
-      } else {
-        notFoundChannel();
-      }
-    } else {
-      notFoundChannel();
+      res.status(400).end();
     }
-  })
-  .post((req, res) => {
-    if (req.body.channelTitle) {
-      let channel = req.body;
-      db.set(id++, channel);
+  )
+  .post(
+    [
+      body("userId").notEmpty().isInt().withMessage("숫자를 입력!"),
+      body("name").notEmpty().isString().withMessage("문자 입력 필요!"),
+    ], // 비면 안되고 정수여야한다.
+    (req, res) => {
+      const err = validationResult(req);
 
-      res.status(201).json({
-        message: `${db.get(id - 1).channelTitle}채널을 응원합니다.`,
-      });
-    } else {
-      res.status(400).json({
-        message: "요청 값을 제대로 보내주세요.",
+      if (!err.isEmpty()) return res.status(400).end(err.array()); // 함수를 종료하며 밑을 읽지않고 끝낸다.
+
+      const { name, userId } = req.body;
+      let sql = `INSERT INTO channels (name, user_id) VALUES (?,?)`;
+      let values = [name, userId];
+
+      conn.query(sql, values, function (err, results) {
+        if (err) return res.status(400).end();
+        res.status(201).json(results);
       });
     }
-  }); // 채널 개별 생성 = db에 저장
+  );
 
 router
   .route("/:id")
-  .get((req, res) => {
+  .get(param("id".notEmpty().withMessage("채널id 필요")), (req, res) => {
+    const err = validationResult(req);
+
+    if (!err.isEmpty()) return res.status(400).end(err.array());
+
     let { id } = req.params;
     id = parseInt(id);
 
-    let channel = db.get(id);
-    if (channel) {
-      res.status(200).json(channel);
-    } else {
-      notFoundChannel();
-    }
+    let sql = `SELECT * FROM channels WHERE id = ?`;
+
+    conn.query(sql, id, function (err, results) {
+      if (err) return res.status(400).end();
+
+      if (results.length) res.status(200).json(results);
+      else notFoundChannel(res);
+    });
   })
-  .put((req, res) => {
+  .put(
+    [
+      param("id".notEmpty().withMessage("채널id 필요")),
+      body("name").notEmpty().isString.withMessage("채널명 오류"),
+    ],
+    (req, res) => {
+      const err = validationResult(req);
+
+      if (!err.isEmpty()) return res.status(400).end(err.array());
+
+      let { id } = req.params;
+      id = parseInt(id);
+      let { name } = req.body;
+
+      let sql = `UPDATE channels SET name = ? WHERE id = ?`;
+      let values = [name, id];
+
+      conn.query(sql, values, function (err, results) {
+        if (err) return res.status(400).end();
+
+        if (results.affectedRows == 0) {
+          return res.status(400).end();
+        } else {
+          res.status(200).json(results);
+        }
+      });
+    }
+  )
+  .delete(param("id".notEmpty().withMessage("채널id 필요")), (req, res) => {
+    const err = validationResult(req);
+
+    if (!err.isEmpty()) return res.status(400).end(err.array());
+
     let { id } = req.params;
     id = parseInt(id);
 
-    let channel = db.get(id);
-    let oldTitle = channel.channelTitle;
-    if (channel) {
-      let newTitle = req.body.channelTitle;
+    let sql = `DELETE FROM channels WHERE id = ?`;
 
-      channel.channelTitle = newTitledb.set(id, channel);
-
-      res.json({
-        message: `채널명이 ${oldTitle}에서 ${newTitle}로 수정되었습니다.`,
-      });
-    } else {
-      notFoundChannel();
-    }
-  })
-  .delete((req, res) => {
-    let { id } = req.params;
-    id = parseInt(id);
-
-    let channel = db.get(id);
-    if (channel) {
-      db.delete(id);
-
-      res.status(200).json({
-        message: `${channel.channelTitle}이 정상적으로 삭제되었습니다.`,
-      });
-    } else {
-      notFoundChannel();
-    }
+    conn.query(sql, id, function (err, results) {
+      if (err) return res.status(400).end();
+      res.status(200).json(results);
+    });
   });
 
-function notFoundChannel() {
+function notFoundChannel(res) {
   res.status(404).json({
     message: "채널 정보를 찾을 수 없습니다.",
   });
--- routes/users.js
+++ routes/users.js
@@ -2,55 +2,31 @@ const express = require("express");
 const router = express.Router();
 const conn = require("../mariadb");
 
-router.use(express.json()); // http 외 모듈 'json'
-
-let db = new Map();
-let id = 1; // 하나의 객체를 유니크하게 구별하기 위함
+router.use(express.json());
 
 // 로그인
 router.post("/login", function (req, res) {
-  // id가 DB에 저장된 회원인지 확인하셔야
-  const { userId, password } = req.body;
+  const { email, password } = req.body;
 
-  let loginUser = {};
+  let sql = `SELECT * FROM users WHERE email = ?`;
 
-  db.forEach(function (user, id) {
-    if (user.userId === userId) {
-      loginUser = user;
-    }
-  });
+  conn.query(sql, email, function (err, results) {
+    let loginUser = results[0];
 
-  // userId 값을 못 찾았으면...
-  if (isExist(loginUser)) {
-    if (user.password == password) {
+    if (loginUser && loginUser.password == password) {
       res.status(200).json({
         message: `${loginUser.name}님 로그인 되었습니다.`,
       });
     } else {
-      res.status(400).json({
-        message: `비밀번호가 틀렸습니다.`,
+      res.status(404).json({
+        message: `이메일 또는 비밀번호가 틀렸습니다.`,
       });
     }
-  } else {
-    res.status(400).json({
-      message: `회원 정보가 없습니다.`,
-    });
-  }
-
-  // pwd도 맞는지 비교
+  });
 });
 
-function isExist(obj) {
-  if (Object.keys(obj).length) {
-    return true;
-  } else {
-    return false;
-  }
-}
 // 회원가입
 router.post("/join", function (req, res) {
-  console.log(req.body);
-
   if (req.body == {}) {
     res.status(400).json({
       message: `입력 값을 다시 확인해주세요.`,
@@ -58,13 +34,12 @@ router.post("/join", function (req, res) {
   } else {
     const { email, name, password, contact } = req.body;
 
-    conn.query(
-      `INSERT INTO users (email,name,password,contact) VALUES (?,?,?,?)`,
-      [email, name, password, contact],
-      function (err, results, fields) {
-        res.status(201).json(results);
-      }
-    );
+    let sql = `INSERT INTO users (email,name,password,contact) VALUES (?,?,?,?)`;
+    let values = [email, name, password, contact];
+
+    conn.query(sql, values, function (err, results) {
+      res.status(201).json(results);
+    });
   }
 });
 
@@ -72,28 +47,19 @@ router
   .route("/users")
   .get(function (req, res) {
     let { email } = req.body;
-    conn.query(
-      `SELECT * FROM users WHERE email = ?`,
-      email,
-      function (err, results, fields) {
-        res.status(200).json(results);
-      }
-    );
+
+    let sql = `SELECT * FROM users WHERE email = ?`;
+    conn.query(sql, email, function (err, results) {
+      res.status(200).json(results);
+    });
   })
   .delete(function (req, res) {
-    let { userId } = req.body;
+    let { email } = req.body;
+    let sql = `DELETE FROM users WHERE email = ?`;
 
-    const user = db.get(userId);
-    if (user) {
-      db.delete(id);
-      res.status(200).json({
-        message: `${user.name}님 다음에 또 뵙겠습니다.`,
-      });
-    } else {
-      res.status(404).json({
-        message: "회원 정보가 없습니다.",
-      });
-    }
+    conn.query(sql, email, function (err, results) {
+      res.status(200).json(results);
+    });
   });
 
 module.exports = router;
